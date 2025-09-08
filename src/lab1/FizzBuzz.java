a9daa78f187b06da182780aa5fe0f16d65dd36d7
Linda han <hanxien24@gmail.com>
2025-09-08 18:28:27 -0400

    first commit

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,9 +6,10 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i < 100; i++) {
-
+            int i = 1;  // initialize counter
+            while (i < 100) {
+            // Find out which numbers divide i.
+            //for (int i = 1; i < 100; i++) {
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
@@ -31,6 +32,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
