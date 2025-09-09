89762be0127e4d7c24e53795f93a78c16c17ea8c
Linda han <hanxien24@gmail.com>
2025-09-09 15:24:08 -0400

    fifth

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -1,8 +1,22 @@
 package lab1;
 
 public class Reduce {
-    public static int reduce(int n) {
-        // TODO: Replace with the actual logic later
-        return 0;
+
+    public static void main(String[] args) {
+        System.out.println(reduce(100));
     }
-}
+
+    // helper method that does the reduce steps
+    public static int reduce(int n) {
+        int steps = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            }
+            else {n = n - 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+    }
